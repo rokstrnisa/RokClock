ab0b63c67693211345263c266c0d4c67eca3fc99
Rok Strniša <rok.strnisa@citrix.com>
2010-09-23 15:31:42 +0100

    Main functionality done.

--- src/timelog/Main.java
+++ src/timelog/Main.java
@@ -1,6 +1,8 @@
 package timelog;
 
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.awt.Color;
 import java.awt.GridLayout;
@@ -10,26 +12,45 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
 
 @SuppressWarnings("serial")
 public class Main extends JFrame {
-	private static final Color defaultColor = Color.GREEN;
-	private static final Color activeColor = Color.RED;
-	private static final String projFilename = "projects.txt";
+	private final String newline = System.getProperty("line.separator");
+	private final String configFilename = "config.txt";
+	private final Properties config = new Properties();
+	private final int intervalInSeconds;
+	private final String projectsFilename, logFilename;
+	private final Color defaultColor, activeColor, semiActiveColor;
+	private final boolean minimise;
 	private final Map<String, String> projects;
+	private final DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
-	private boolean running = false;
+	private enum State {STOPPED, RUNNING, AUTOMATIC}
+	
+	private State state = State.STOPPED;
+	private Timer timer;
 	private long startTime = 0;
 	private String runningProject = null;
 	private String runningSubProject = null;
 	private JButton runningButton = null;
 
 	private Main() throws IOException {
-		setTitle("Time Log");
-		setLocation(400, 400);
+		// loading settings
+		config.load(new FileInputStream(configFilename));
+		setTitle(get("title", "Time Log"));
+		setLocation(get("locX", 400), get("locY", 400));
+		intervalInSeconds = get("intervalInSeconds", 3600);
+		defaultColor = get("defaultColor", Color.GREEN);
+		activeColor = get("activeColor", Color.RED);
+		semiActiveColor = get("semiActiveColor", Color.CYAN);
+		minimise = get("behaviour", "minimise").equals("minimise");
+		logFilename = get("logFilename", "log.txt");
+		// loading projects + GUI
 		getContentPane().setLayout(new GridLayout(0, 1));
+		projectsFilename = get("projectsFilename", "projects.txt");
 		projects = loadProjectNames();
 		for (String project : projects.keySet())
 			addButton(project, projects.get(project));
@@ -37,17 +58,36 @@ public class Main extends JFrame {
 		pack();
 		setAlwaysOnTop(true);
 		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				stopRecording();
+			public void windowClosing(WindowEvent ev) {
+				try {stopRecording();}
+				catch (IOException ex) {ex.printStackTrace();}
 				System.exit(0);
 			}
 		});
+		timer = createTimer();
 		setVisible(true);
+	}
+	
+	private String get(String key, String defaultValue) {
+		String v = config.getProperty(key);
+		return v == null ? defaultValue : v;
+	}
+
+	private int get(String key, int defaultValue) {
+		String v = config.getProperty(key);
+		return v == null ? defaultValue : Integer.parseInt(v);
+	}
+	
+	private Color get(String key, Color defaultValue) {
+		String v = config.getProperty(key);
+		if (v == null) return defaultValue;
+		String[] rgb = v.split(",");
+		return new Color(Integer.parseInt(rgb[0]), Integer.parseInt(rgb[1]), Integer.parseInt(rgb[2]));
 	}
 	
 	private Map<String, String> loadProjectNames() throws IOException {
 		Map<String, String> result = new LinkedHashMap<String, String>();
-		BufferedReader br = new BufferedReader(new FileReader(projFilename));
+		BufferedReader br = new BufferedReader(new FileReader(projectsFilename));
 		String line;
 		while ((line = br.readLine()) != null) {
 			line = line.trim();
@@ -65,23 +105,38 @@ public class Main extends JFrame {
 		return result;
 	}
 
-	private void startRecording(JButton b, String project, String subProject) {
-		if (running) stopRecording();
+	private void startRecording(JButton b, String project, String subProject) throws IOException {
+		if (state != State.STOPPED) stopRecording();
 		startTime = System.currentTimeMillis();
 		runningProject = project;
 		runningSubProject = subProject;
 		runningButton = b;
 		runningButton.setBackground(activeColor);
-		running = true;
+		state = State.RUNNING;
 	}
 	
-	private void stopRecording() {
-		if (!running) return;
-		long endTime = System.currentTimeMillis();
-		System.err.println(runningProject + ", " + runningSubProject + ", " + startTime + ", " + endTime);
+	private void stopRecording() throws IOException {
+		switch (state) {
+			case STOPPED: return;
+			case AUTOMATIC:
+				timer.stop();
+				break;
+			case RUNNING:
+				long endTime = System.currentTimeMillis();
+				writeLogEntry(runningProject, runningSubProject, startTime, endTime);
+		}
 		runningButton.setBackground(defaultColor);
-		runningButton = null;
-		running = false;
+		state = State.STOPPED;
+	}
+	
+	private void writeLogEntry(String project, String subProject, long startTime, long endTime) throws IOException {
+		String startTimeS = df.format(new Date(startTime));
+		String endTimeS = df.format(new Date(endTime));
+		String entry = runningProject + ", " + runningSubProject + ", " + startTimeS + ", " + endTimeS;
+		System.err.println(entry);
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(logFilename, true)));
+		out.write(entry + newline);
+		out.close();
 	}
 	
 	private void addButton(final String project, final String superProject) {
@@ -91,16 +146,15 @@ public class Main extends JFrame {
 		b.setHorizontalAlignment(SwingConstants.LEFT);
 		b.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (b == runningButton) {
-					stopRecording();
-				} else {
+				try {
 					String mainProject = superProject == null ? project : superProject;
 					String subProject = superProject == null ? "" : project;
+					if (state == State.AUTOMATIC) state = State.RUNNING;
 					startRecording(b, mainProject, subProject);
-					// setVisible(false); // if we don't want it in toolbar
-					setExtendedState(ICONIFIED);
-					waitThenPopup();
-				}
+					if (minimise) setExtendedState(ICONIFIED);
+					else setVisible(false);
+					timer.restart();
+				} catch (IOException ex) {showProblem(ex);}
 			}
 		});
 		add(b);
@@ -111,25 +165,37 @@ public class Main extends JFrame {
 		b.setBackground(Color.BLACK);
 		b.setForeground(Color.GRAY);
 		b.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}			
+			public void actionPerformed(ActionEvent e) {
+				try {stopRecording();}
+				catch (IOException ex) {showProblem(ex);}
+			}
 		});
 		add(b);
 	}
 	
-	private void waitThenPopup() {
-		int delay = 5000;
-		ActionListener taskPerformer = new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				// setVisible(true); // if we don't want it in toolbar
-				setExtendedState(NORMAL);
+	private Timer createTimer() {
+		Timer t = new Timer(intervalInSeconds * 1000, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if (state == State.AUTOMATIC) {
+						stopRecording();
+					} else if (state == State.RUNNING) {
+						stopRecording();
+						startRecording(runningButton, runningProject, runningSubProject);
+						state = State.AUTOMATIC;
+						if (runningButton != null)
+							runningButton.setBackground(semiActiveColor);
+					}
+				} catch (IOException ex) {showProblem(ex);}
+				if (minimise) setExtendedState(NORMAL);
+				else setVisible(true);
 			}
-		};
-		Timer t = new Timer(delay, taskPerformer);
-		t.setRepeats(false);
-		t.start();
+		});
+		return t;
+	}
+	
+	private void showProblem(Exception e) {
+		JOptionPane.showMessageDialog(this, "A problem has occurred: " + e.getMessage());
 	}
 	
 	public static void main(String[] args) throws IOException {
